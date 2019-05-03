acea6d3082d5762f0955a5093e0ca0a8ec459f85
Pedro Oliveira <pedromgdoliveira@gmail.com>
2019-05-03 13:48:30 +0100

    ahhhhhh pois

--- sv.c
+++ sv.c
@@ -1,27 +1,27 @@
-#include <stdio.h> 
-#include <string.h> 
-#include <fcntl.h> 
-#include <sys/stat.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 
 int main(){
-    char msg[80];
+    char *msg;
     /* Criação de pipes */
     mkfifo("ServerCall",0666);
     mkfifo("ClientCall",0666);
 
     int fd;
+    fd = open("ServerCall",O_WRONLY);
     while(1){
-        fd = open("ServerCall",O_WRONLY);
-        //read(0,msg,80);
-        fgets(msg, 80, stdin); 
-
-        write(fd, msg, strlen(msg)+1); 
-        close(fd); 
-        
+        msg = malloc(100 * sizeof(char *));
+        read(0,msg,80);
+        write(fd, msg, strlen(msg)+1);
+        free(msg);
     }
-    
-    
+
+    close(fd);
+
 }

177c50e632d0bed90d3ad309dc2707b3a58f2b0d
António Santos <antoniomsantos99@gmail.com>
2019-05-05 14:50:51 +0100

    Comandos do main feitos (Problema nos ifs mas nada de muito importante)

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -20,30 +20,37 @@ int contaPal (char s[]) {
     return count;
 }
 
+int isnumber(char c[100])
+{
+  int i;
+  for(i=0;c[i];i++){
+    if (isdigit(c[i]) == 0) return 0;
+  }
+  return 1;
+}
+
 int main(){
     // argumentos para serem utilizados no sscanf
     char input[100];
-    char arg1;
-    char *arg2 = malloc(100*sizeof(char *));
-    int arg3;
+    char arg1,arg2[100],arg3[100];
+    //char *arg2 = malloc(100*sizeof(char *));
     // loop infinito para tar sempre a pedir novo input (quando ouver input errado dá quits)
     while(1){
       read(0, input, 100);
       if (contaPal(input)>2){
-      sscanf(input, "%c %s %d", &arg1, arg2, &arg3);
-      if(arg1=='i'){
-        addString(arg2,arg3);
+      sscanf(input, "%c %s %s", &arg1, arg2, arg3);
+      if(arg1=='i' && isnumber(arg3)){
+        addString(arg2,atoi(arg3));
       } //comando i para introduzir novo produto
-      else if(arg1=='n'){
-        sscanf(input, "%c %d %s", &arg1, &arg3, arg2);
-        mudaNome(arg3,arg2);
+      else if(arg1=='n' && isnumber(arg2)){
+        sscanf(input, "%c %s %s", &arg1, arg2, arg3);
+        mudaNome(atoi(arg2),arg3);
       } // comando p muda o preco, primeiro local, depois preco novo
-      else if(arg1=='p'){
-        int arg4;
-        sscanf(input, "%c %d %d", &arg1, &arg3, &arg4);
-        mudaPreco(arg3,arg4);
-      } // qaualquer outro comando dá quit
-      else return;
+      else if(arg1=='p'&& isnumber(arg2) && isnumber(arg3)){
+        sscanf(input, "%c %s %s", &arg1, arg2, arg3);
+        mudaPreco(atoi(arg2),atoi(arg3));
+      }
+      else printf("Formato errado!");
     }
     else printf("Vai pa puta que te pariu tu e os formatos errados\n");
     }

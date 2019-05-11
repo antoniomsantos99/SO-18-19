e0afe4b42afd846f2a3ab824fdf6ab45887f2f70
Pedro Oliveira <pedromgdoliveira@gmail.com>
2019-05-11 16:20:34 +0100

    casos testes agora pode receber argumentos do numero de execuções

--- criaCasosTeste.c
+++ criaCasosTeste.c
@@ -9,37 +9,69 @@
 #define NR_EXECS 100
 #define MAX_STOCKS 100
 
-int main(){
+int main(int argc, char *argv[]){
   //contar as linhas do ficheiro para saber em quais pode adicioanr stock
   int maxLinha = countLines("ficheirosTexto/Artigos.txt");
   pid_t pid;
-  //cria os forks
-  for(int i = 0; i < NR_EXECS; i++){
-    pid = fork();
-    if(pid == 0){
-      //randomizar seed
-      srand(time(NULL) ^ (getpid()<<16));
-      int linha = 1+rand()%maxLinha;
+  if(argc==2){//caso recebe argumentos executa o numero de vezes no argumento
+    int execs = atoi(argv[1]);
+    for(int i = 0; i < execs; i++){
+      pid = fork();
+      if(pid == 0){
+        //randomizar seed
+        srand(time(NULL) ^ (getpid()<<16));
+        int linha = 1+rand()%maxLinha;
 
-      srand(time(NULL) ^ (getpid()<<16));
-      int stock = rand()%MAX_STOCKS-20;
+        srand(time(NULL) ^ (getpid()<<16));
+        int stock = rand()%MAX_STOCKS-20;
 
 
-      //transforma os numeros numa string
-      char sLinha[100];
-      sprintf(sLinha,"%d",linha);
-      char sStock[100];
-      sprintf(sStock,"%d",stock);
+        //transforma os numeros numa string
+        char sLinha[100];
+        sprintf(sLinha,"%d",linha);
+        char sStock[100];
+        sprintf(sStock,"%d",stock);
 
-      //executa a cv.c com os argumentos randomizados
-      char *args[] = {"./cv",sLinha,sStock,NULL};
-      execvp (args[0],args);
-      _exit(0);
-    }else{
-      //espera que o filho acabe para criar novo filho e executar o programa de novo
-      wait(NULL);
-      //espera um pouco para não dar overload ao pipe (0.02 segundos)
-      usleep(20000);
+        //executa a cv.c com os argumentos randomizados
+        char *args[] = {"./cv",sLinha,sStock,NULL};
+        execvp (args[0],args);
+        _exit(0);
+      }else{
+        //espera que o filho acabe para criar novo filho e executar o programa de novo
+        wait(NULL);
+        //espera um pouco para não dar overload ao pipe (0.02 segundos)
+        usleep(20000);
+      }
+    }
+  }else{//caso seja iniciado normalmente executa com o numero de execuções defenido acima
+    for(int i = 0; i < NR_EXECS; i++){
+      //cria os forks
+      pid = fork();
+      if(pid == 0){
+        //randomizar seed
+        srand(time(NULL) ^ (getpid()<<16));
+        int linha = 1+rand()%maxLinha;
+
+        srand(time(NULL) ^ (getpid()<<16));
+        int stock = rand()%MAX_STOCKS-20;
+
+
+        //transforma os numeros numa string
+        char sLinha[100];
+        sprintf(sLinha,"%d",linha);
+        char sStock[100];
+        sprintf(sStock,"%d",stock);
+
+        //executa a cv.c com os argumentos randomizados
+        char *args[] = {"./cv",sLinha,sStock,NULL};
+        execvp (args[0],args);
+        _exit(0);
+      }else{
+        //espera que o filho acabe para criar novo filho e executar o programa de novo
+        wait(NULL);
+        //espera um pouco para não dar overload ao pipe (0.02 segundos)
+        usleep(20000);
+      }
     }
   }
 }
